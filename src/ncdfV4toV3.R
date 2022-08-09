9dd6a53213a59cfde0c0a4908643350cf8310bb0
clayton-aldern <clayton.aldern@gmail.com>
2022-08-09 12:29:54 -0700

    clean up for github

--- src/ncdfV4toV3.R
+++ src/ncdfV4toV3.R
@@ -1,27 +1,60 @@
 library(ncdf4)
 library(RColorBrewer)
+library(OpenImageR)
 
 setwd("~/Developer/gristy/drought-page")
 
-i <- nc_open("cmorph_spi_gamma_30_day_latest.nc")
+rm(list=ls())
+fname <- "cmorph_spi_gamma_90_day_latest.nc"
+url <- paste0("https://www.ncei.noaa.gov/pub/data/nidis/test/cmorph/",
+              fname)
+# tryCatch()
+download.file(url, fname)
+i <- nc_open(fname)
 
-lat <- ncdim_def("lat",
-                 "degrees_north",
-                 ncvar_get(i,"lat"))
+# lon data are shifted 180º relative to reader default in D3_netcdfjs notebook,
+# but we'll do that transformation in Observable
 lon <- ncdim_def("lon",
                  "degrees_east",
-                 ncvar_get(i,"lon"))
+                 seq(-179.5,179.5,1)) #downsampled version
+                 # seq(.5,359.5,1)) #downsampled version
+                 # ncvar_get(i,"lon"))
+
+# for lat data, we'll pad with zeros here
+lat <- ncdim_def("lat",
+                 "degrees_north",
+                 seq(-89.5,89.5,1)) #downsampled version
+                 # seq(-89.875,89.875,.25))
+                 # ncvar_get(i,"lat"))
+
 time <- ncdim_def("time",
                   "seconds since 1970-01-01",
                   ncvar_get(i,"time"))
-spi_gamma_30_day <- ncvar_def("spi_gamma_30_day",
+spi_gamma_90_day <- ncvar_def("spi_gamma_90_day",
                               "",
-                              dim=list(lat,lon,time),
-                              longname="Standardized Precipitation Index (Gamma), 30-day")
-spi <- ncvar_get(i,"spi_gamma_30_day")
-# spi[is.na(spi)] <- 0
+                              dim=list(lon,lat,time),
+                              longname="Standardized Precipitation Index (Gamma), 90-day")
+spi <- ncvar_get(i,"spi_gamma_90_day")
+spi[is.na(spi)] <- 0
+spi <- cbind(matrix(0,1440,120),spi,matrix(0,1440,120))
+# spi <- as.data.frame(spi)
 
-o <- nc_create("spi.nc", spi_gamma_30_day)
-ncvar_put(o, "spi_gamma_30_day", spi)
+# implement drought severity ratings
+spi[spi >= -.4] <- 0
+spi[spi <= -0.5 & spi > -0.8] <- 0
+spi[spi <= -0.8 & spi > -1.3] <- 1
+spi[spi <= -1.3 & spi > -1.6] <- 2
+spi[spi <= -1.6 & spi > -2] <- 3
+spi[spi <= -2] <- 4
+
+# downsample original .nc
+spiDown <- resizeImage(spi,
+                       360,
+                       180,
+                       method = "nearest",
+                       normalize_pixels = FALSE)
+
+o <- nc_create("spiDown.nc", spi_gamma_90_day)
+ncvar_put(o, "spi_gamma_90_day", spiDown)
 nc_close(i)
 nc_close(o)
